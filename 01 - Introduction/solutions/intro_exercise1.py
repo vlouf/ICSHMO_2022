e318ca1fe4c5507565a84ac55bf3721066b57e63
Valentin Louf <valentin.louf@bom.gov.au>
2022-02-07 16:55:11 +1100

    Update part 2

--- 01 - Introduction/solutions/intro_exercise1.py
+++ 01 - Introduction/solutions/intro_exercise1.py
@@ -1,10 +1,9 @@
-step = 110
-datetime = pd.Timestamp(dset.time[step].to_pandas())
+datetime = pd.Timestamp(dset.time[0].to_pandas())
 
 fig = plt.figure(figsize=(12, 10))
 ax1 = plt.subplot(1, 1, 1, projection=projection)
 ax1.coastlines('10m')
-im = ax1.pcolormesh(x, y, (10 ** (refl[step, :, :] / 10) / 200) ** (1 / 1.6), vmin=0, cmap='Blues')
+im = ax1.pcolormesh(x, y, (10 ** (refl[5, :, :] / 10) / 200) ** (1 / 1.6), vmin=0, cmap='Blues')
 
 # Plotting the range rings.
 theta = np.linspace(0, 2 * np.pi)
@@ -12,5 +11,5 @@ for r in [50e3, 100e3, 150e3]:
     ax1.plot(r * np.cos(theta), r * np.sin(theta), 'k', linewidth=1, alpha=0.75)
 
 plt.colorbar(im, ax=ax1, label='Rainfall rate (mm/h)')
-plt.title(f'CPOL - {datetime.isoformat()} UTC')
+plt.title(f'OceanPOL - {datetime.isoformat()} UTC')
 plt.show()
